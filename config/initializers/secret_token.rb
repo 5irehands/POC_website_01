e325109a3129e7068ad1c5845be85198ea2b87ab
nono <5irehands@gmail.com>
2013-09-17 19:23:32 +0800

    nono first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+POCWebsite01::Application.config.secret_token = '49f081dd3c48e3e486def85d12494382159c15ef2c38f286ee09c12eaefafd29d9dbd7f4cb0a1b0110a42afdbdc3352462223435f5001644f4621edb01914627'
